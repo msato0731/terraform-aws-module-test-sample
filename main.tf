4f5a937c7718262f81efcd7d909c3bbd4ac77925
msato0731 <rhcq.3127@gmail.com>
2023-10-12 16:25:07 +0900

    add: sqs

---
+++ main.tf
@@ -0,0 +1,13 @@
+variable "region" {}
+variable "prefix" {}
+variable "name" {}
+
+
+resource "aws_sqs_queue" "this" {
+  name                    = "${prefix}-${name}"
+  sqs_managed_sse_enabled = true
+}
+
+output "quque_url" {
+  value = aws_sqs_queue.this.url
+}
